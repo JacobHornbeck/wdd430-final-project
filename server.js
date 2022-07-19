8e3323794d42bd57bfcc0d2e2db7d167fa145934
hor20009 <hor20009@byui.edu>
2022-07-18 21:04:59 -0600

    final-project

---
+++ server.js
@@ -0,0 +1,46 @@
+// Get dependencies
+const express = require('express');
+const mongoose = require('mongoose')
+const http = require('http');
+
+const port = process.env.PORT || '3000';
+
+// import the routing file to handle the default (index) route
+const solveRoutes = require('./server/routes/solves')
+
+mongoose.connect('mongodb+srv://nodejs-access:gNiJjNn5Uxwm8H1j@wdd430-final-project.s5udc.mongodb.net/final-project', { useNewUrlParser: true }, (err, res) => {
+    if (err) console.log('Connection failed: ' + err)
+    else console.log('Connected to database!')
+})
+
+const expressApp = express(); // create an instance of express
+
+// Tell express to use the following parsers for POST data
+expressApp
+    .use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept'
+        );
+        res.setHeader(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+        );
+        next();
+    })
+    // .use(express.static(path.join(__dirname, 'dist/wdd430-final-project')))
+    .use('/solves', solveRoutes)
+    .use((req, res) => {
+        res.status(404).json('That route isn\'t set, please check the route and try again')
+    })
+    .set('port', port);
+
+// Create HTTP server.
+const server = http.createServer(expressApp);
+
+server.listen(port, function () {
+    console.log('API running on localhost: ' + port)
+});
---
+++ server/models/solve.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+/* { id: String, startTime: Date, endTime: Date, scramble: String, comment: String } */
+/* Methods: Schema.getId() and Solve.updateComment() */
+
+const solveSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    endTime: {
+        type: Date,
+        required: true
+    },
+    scramble: {
+        type: String,
+        required: false
+    },
+    comment: {
+        type: String,
+        required: false,
+        default: ''
+    }
+})
+
+solveSchema.statics.getId = async function() {
+    let solves = Array.from(await this.find())
+    return solves.reduce((p, c) => {
+        if (c.id == p) return p+1
+        else return p
+    }, 1)
+}
+
+solveSchema.methods.updateComment = function(newComment) {
+    this.comment = newComment
+    return this.save()
+}
+
+module.exports = mongoose.model('Solve', solveSchema)
